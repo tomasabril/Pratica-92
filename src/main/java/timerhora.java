ad52c209268cc24cfb36cae89c270eabed4cc9c4
tomas <>
2014-09-01 12:12:22 -0300

    fim da aula

--- src/main/java/timerhora.java
+++ src/main/java/timerhora.java
@@ -1,28 +1,36 @@
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
 import java.util.TimerTask;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
- * @author a1013343
+ * @author a1013343 Tomás Abril
  */
 public class timerhora extends TimerTask {
 
     private final Date horaAtual = new Date();
     private final SimpleDateFormat sdf = new SimpleDateFormat("'Hora:' HH:mm:ss");
+    private Timer timerminutoimpar;
+    private TimerTask imparTask;
+
+    public timerhora(Timer timerminutoimpar) {
+        this.timerminutoimpar = timerminutoimpar;
+    }
 
     @Override
     public void run() {
         horaAtual.setTime(System.currentTimeMillis());
         System.out.println(sdf.format(horaAtual));
-        if(horaAtual.getMinutes()%2==0) {
-            
+        if ((horaAtual.getTime() % 120000) >= 60000) {
+            imparTask = new timerminimpar();
+            timerminutoimpar.scheduleAtFixedRate(imparTask, horaAtual, 10000);
+
+        } else {
+            if (imparTask != null) {
+                imparTask.cancel();
+            }
         }
     }
 
